3300afb6e061eacd3cf020eb410446bd1b525c7d
Riya Amemiya <riyaamemiya@gmail.com>
2024-03-17 00:31:41 +0900

    refactor(src/app/r/[id]): 不要なテスト用h1タグを削除

--- src/app/r/[id]/client.tsx
+++ src/app/r/[id]/client.tsx
@@ -24,7 +24,6 @@ export const RedirectPageClient = ({
     <Card className="max-w-md mx-auto bg-slate-50 rounded-xl shadow-md md:max-w-2xl w-full md:p-10 p-6">
       <CardContent>
         <div className="flex flex-col items-center justify-center">
-          <h1>test</h1>
           {pathOnly}
         </div>
       </CardContent>
